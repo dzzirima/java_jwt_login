918e6c11281f92458803a1089df94b74f42300de
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-08-25 10:58:16 +0200

    cors configuration done

---
+++ src/main/java/com/zirima/JwtAuthFinalApplication.java
@@ -0,0 +1,13 @@
+package com.zirima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zirima/JwtAuthFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zirima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
